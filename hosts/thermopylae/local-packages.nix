8fda50696ae370cd8adfa02f3c3846f878ec2b2a
Alejandro Moreno <ale.moreno991@gmail.com>
2025-01-25 17:20:00 +0800

    Remove system packages

--- hosts/thermopylae/local-packages.nix
+++ hosts/thermopylae/local-packages.nix
@@ -1,10 +1,5 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    gcc
-    kdenlive
-    # jetbrains.pycharm-professional
-    # jre8
-    # qemu
-    # quickemu
+    # System packages
   ];
 }
