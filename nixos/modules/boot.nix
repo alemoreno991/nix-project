d5cb96ce5aefa2a0beab2fb9f558032351eba1d9
Alejandro Moreno <ale.moreno991@gmail.com>
2025-02-02 17:07:32 +0800

    Changing the console mode in the bootloader

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -2,6 +2,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.loader.systemd-boot.consoleMode = "auto";
 
   boot.initrd.luks.devices."luks-f3898c3b-e49d-4ebe-bfaf-bb87e0b5dd93".device = "/dev/disk/by-uuid/f3898c3b-e49d-4ebe-bfaf-bb87e0b5dd93";
 
