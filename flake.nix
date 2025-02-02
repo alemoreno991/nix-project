0e508a24032bcb63865b5b0444573d8906cc4dbd
Alejandro Moreno <ale.moreno991@gmail.com>
2025-02-02 21:37:44 +0800

    Adding disko

--- flake.nix
+++ flake.nix
@@ -5,9 +5,18 @@
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
     };
 
     stylix.url = "github:danth/stylix";
@@ -34,7 +43,9 @@
       };
 
       modules = [
+        inputs.disko.nixosModules.default
         ./hosts/${hostname}/configuration.nix
+        
       ];
     };
 
