83ac161ae9dfff1cafe49a306670ad29d5174569
Alejandro Moreno <ale.moreno991@gmail.com>
2025-03-23 19:49:37 +0800

    Adding quarto

--- home-manager/modules/nvim/default.nix
+++ home-manager/modules/nvim/default.nix
@@ -26,6 +26,7 @@
     # NOTE: Configure your own plugins `see https://nix-community.github.io/nixvim/`
     # Add your plugins to ./plugins/custom/plugins and import them below
     ./plugins/custom/plugins/molten.nix
+    ./plugins/custom/plugins/quarto.nix
   ];
 
   /*
---
+++ home-manager/modules/nvim/plugins/custom/plugins/quarto.nix
@@ -0,0 +1,41 @@
+{
+  programs.nixvim = {
+    plugins.quarto = {
+      enable = true;
+
+      settings = {
+        lspFeatures = {
+          enabled = true;
+          languages = [ 
+            "python"
+          ];
+
+          chunks = "all";
+
+          diagnostics = {
+            enabled = true;
+            triggers = [ 
+              "BufWritePost" 
+            ];
+          },
+
+          completion.enabled = true;
+        };
+
+        keymap = {
+          # NOTE: setup your own keymaps:
+          hover = "H";
+          definition = "gd";
+          rename = "<leader>rn";
+          references = "gr";
+          format = "<leader>gf";
+        };
+
+        codeRunner = {
+          enabled = true;
+          default_method = "molten";
+        };
+      };
+    };
+  };
+}
