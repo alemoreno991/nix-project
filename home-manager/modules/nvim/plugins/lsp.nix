91334a5646f095520f95ca0e5ca488e306237bd1
Alejandro Moreno <ale.moreno991@gmail.com>
2025-03-24 22:00:37 +0800

    Add C/C++ LSP

--- home-manager/modules/nvim/plugins/lsp.nix
+++ home-manager/modules/nvim/plugins/lsp.nix
@@ -70,13 +70,13 @@
       #        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       servers = {
         clangd = {
-         enable = true;
-        }
+          enable = true;
+        };
         # gopls = {
         #  enable = true;
         #}
         pyright = {
-         enable = true;
+          enable = true;
         };
         # rust_analyzer = {
         #  enable = true;
